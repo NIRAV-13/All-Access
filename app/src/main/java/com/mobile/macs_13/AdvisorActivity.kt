ec36e60c746369f6f72762e1fe4a291ed7966c9b
AnkushMudgal <ankushmudgal.mail@gmail.com>
2022-11-25 17:42:38 -0400

    Changes to the Advisor home - Moved logic from Fragment to AdvisorActivity.kt

--- app/src/main/java/com/mobile/macs_13/AdvisorActivity.kt
+++ app/src/main/java/com/mobile/macs_13/AdvisorActivity.kt
@@ -2,10 +2,34 @@ package com.mobile.macs_13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AdvisorActivity : AppCompatActivity() {
+
+    private lateinit var adapter: AdvisorHomeAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var accomRequestList: ArrayList<AccomRequest>
+
+    val dummy = arrayListOf<AccomRequest>(
+        AccomRequest(
+            "Test",
+            "Data",
+            "Some details",
+            "jkb"
+        ),
+        AccomRequest("Test 2", "Data", "kjnkfn", "jfnlkvmk")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_advisor)
+
+        val layoutManager = LinearLayoutManager(this.baseContext)
+        recyclerView = findViewById(R.id.advisor_home_rv)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.setHasFixedSize(true)
+        adapter = AdvisorHomeAdapter(dummy)
+        recyclerView.adapter = adapter
     }
 }
