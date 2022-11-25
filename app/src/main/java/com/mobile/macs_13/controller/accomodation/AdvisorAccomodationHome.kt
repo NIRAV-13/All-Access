39e70741cfdb3a179721832604c65f9374fe66c0
Adarsh Iyengar <ad398244@dal.ca>
2022-11-25 17:36:07 -0400

    Accomodation request model updated

---
+++ app/src/main/java/com/mobile/macs_13/controller/accomodation/AdvisorAccomodationHome.kt
@@ -0,0 +1,39 @@
+package com.example.accomodationfeature
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.mobile.macs_13.R
+
+class AdvisorAccomodationHome : AppCompatActivity() {
+
+    private lateinit var studReqBtn : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_advisor_accomodation_home)
+
+        //to remove the action bar
+        supportActionBar?.hide()
+
+        studReqBtn = findViewById(R.id.stud_request_btn)
+        studReqBtn.setOnClickListener{
+
+            val email = "alexjones@dal.ca"
+
+            var db = FirebaseFirestore.getInstance()
+
+            db.collection("")
+
+            // navigate to the advisor requests page
+            val accomIntent = Intent(this@AdvisorAccomodationHome, AdvisorAccomodation::class.java )
+            finish()
+            startActivity(accomIntent)
+        }
+    }
+}
---
+++ app/src/main/java/com/mobile/macs_13/model/AdvisorAccomodationModel.kt
@@ -0,0 +1,12 @@
+package com.mobile.macs_13.model
+
+import java.util.Date
+
+
+data class AdvisorAccomodationModel(var name:String?,
+                                    var impact: String?,
+                                    var email:String?,
+                                    var consent:String?,
+                                    var uid:String?,
+                                    var comments: String?,
+                                    var timeStamp : Date?)
