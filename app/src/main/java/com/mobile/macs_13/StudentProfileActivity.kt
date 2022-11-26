a988f175f7e22a383f4dd2a34fcabbba12b23426
AnkushMudgal <ankushmudgal.mail@gmail.com>
2022-11-26 01:37:43 -0400

    StudentProfile Complete and Integrated.

--- app/src/main/java/com/mobile/macs_13/StudentProfileActivity.kt
+++ app/src/main/java/com/mobile/macs_13/StudentProfileActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
+import com.mobile.macs_13.controller.utils.User
 
 class StudentProfileActivity : AppCompatActivity() {
 
@@ -19,13 +20,13 @@ class StudentProfileActivity : AppCompatActivity() {
         val studentProgram = findViewById<TextView>(R.id.student_profile_program)
 
         Glide.with(this.baseContext)
-            .load("https://firebasestorage.googleapis.com/v0/b/chatapp-11645.appspot.com/o/IMG_20200303_000056.jpg?alt=media&token=1d65b825-cfd9-4d80-b910-8e2080000cda")
-            .centerCrop().placeholder(R.drawable.ic_profile)
+            .load(User.getCurrentUserProfile().imageLink)
+            .centerCrop().placeholder(R.drawable.ic_profile).fallback(R.drawable.ic_profile)
             .into(studentImage);
 
-        studentName.text = "NAME - " + "Ankush Mudgal"
-        studentEmail.text = "EMAIL - " + "test@test.ca"
-        studentPhone.text = "PHONE - " + "67578329189"
-        studentProgram.text = "PROGRAM - " + "MACS"
+        studentName.text = "NAME - " + User.getCurrentUserProfile().name
+        studentEmail.text = "EMAIL - " + User.getCurrentUserProfile().email
+        studentPhone.text = "PHONE - " + User.getCurrentUserProfile().phone
+        studentProgram.text = "PROGRAM - " + User.getCurrentUserProfile().program
     }
 }
