2c644d652ad8c8cbd38ec171648c8f2465d11d3e
meghnak <meghna.kumar@dal.ca>
2022-11-26 12:02:10 -0400

    added back button and action to profile activity

--- app/src/main/java/com/mobile/macs_13/StudentProfileActivity.kt
+++ app/src/main/java/com/mobile/macs_13/StudentProfileActivity.kt
@@ -1,9 +1,12 @@
 package com.mobile.macs_13
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.NonNull
 import com.bumptech.glide.Glide
 import com.mobile.macs_13.controller.utils.User
 
@@ -12,6 +15,9 @@ class StudentProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_profile)
+
+        supportActionBar?.title = "Profile"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val studentImage = findViewById<ImageView>(R.id.student_profile_image)
         val studentName = findViewById<TextView>(R.id.student_profile_name)
@@ -28,5 +34,16 @@ class StudentProfileActivity : AppCompatActivity() {
         studentEmail.text = "EMAIL - " + User.getCurrentUserProfile().email
         studentPhone.text = "PHONE - " + User.getCurrentUserProfile().phone
         studentProgram.text = "PROGRAM - " + User.getCurrentUserProfile().program
+    }
+
+    override fun onOptionsItemSelected(@NonNull item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            android.R.id.home -> {
+                val intent = Intent(this, StudentActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
