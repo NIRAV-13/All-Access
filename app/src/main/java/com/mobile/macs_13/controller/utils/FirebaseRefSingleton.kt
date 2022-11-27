68c1da96ed3a2fe87839c75b6001706e0baa006f
meghnak <meghna.kumar@dal.ca>
2022-11-27 11:07:51 -0400

    added delay in forgot password page and global firebase ref

--- app/src/main/java/com/mobile/macs_13/controller/utils/FirebaseRefSingleton.kt
+++ app/src/main/java/com/mobile/macs_13/controller/utils/FirebaseRefSingleton.kt
@@ -1,5 +1,6 @@
 package com.mobile.macs_13.controller.utils
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -7,9 +8,13 @@ import com.google.firebase.ktx.Firebase
 object FirebaseRefSingleton {
 
     private val firebaseDB = Firebase.firestore
+    private val loginAuth = FirebaseAuth.getInstance()
 
     fun getFirebaseDB(): FirebaseFirestore {
         return firebaseDB
     }
 
+    fun getFirebaseAuth(): FirebaseAuth{
+        return loginAuth
+    }
 }
