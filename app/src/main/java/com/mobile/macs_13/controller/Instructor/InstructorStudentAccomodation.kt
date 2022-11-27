4bdce7532a1a0b890afc4dfa0970e47a0fc035e8
niravradadiya <nr723865@dal.ca>
2022-11-26 22:13:09 -0400

    Added Student Accomodation Page

---
+++ app/src/main/java/com/mobile/macs_13/controller/Instructor/InstructorStudentAccomodation.kt
@@ -0,0 +1,13 @@
+package com.mobile.macs_13.controller.Instructor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobile.macs_13.R
+
+class InstructorStudentAccomodation : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_instructor_student_accomodation)
+
+    }
+}
--- app/src/main/java/com/mobile/macs_13/controller/Instructor/instructorView.kt
+++ app/src/main/java/com/mobile/macs_13/controller/Instructor/instructorView.kt
@@ -1,21 +1,25 @@
 package com.mobile.macs_13.controller.Instructor
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.Spinner
 import com.mobile.macs_13.R
+import com.mobile.macs_13.controller.FileOperation.UploadFile
 
 class instructorView : AppCompatActivity() {
     private lateinit var selectsemdropdown: Spinner
     private lateinit var selectcoursedropdown: Spinner
+    lateinit var  searchbutton : Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_instructor_view)
 
-
+        searchbutton = findViewById(R.id.button6)
 
         selectsemdropdown=findViewById(R.id.spinnerSelectSemester);
         var adaptersem = ArrayAdapter.createFromResource(this, R.array.selectSemester,
@@ -28,5 +32,10 @@ class instructorView : AppCompatActivity() {
             androidx.transition.R.layout.support_simple_spinner_dropdown_item)
         adaptercourse.setDropDownViewResource(androidx.transition.R.layout.support_simple_spinner_dropdown_item)
         selectcoursedropdown.setAdapter(adaptercourse);
+
+        searchbutton.setOnClickListener {
+            val searchDetailsIntent = Intent(this, InstructorCourseView::class.java)
+            startActivity(searchDetailsIntent)
+        }
     }
 }
