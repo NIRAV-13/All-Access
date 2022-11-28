290e2b16e8481ac5476acf951b64b35cf99ca1be
meghnak <meghna.kumar@dal.ca>
2022-11-27 22:27:03 -0400

    fixed home navigation from profile page bug

--- app/src/main/java/com/mobile/macs_13/view/student/StudentProfileActivity.kt
+++ app/src/main/java/com/mobile/macs_13/view/student/StudentProfileActivity.kt
@@ -69,7 +69,11 @@ class StudentProfileActivity : AppCompatActivity() {
         val navigationView = findViewById<NavigationView>(R.id.navigationView)
 
         navigationView.setNavigationItemSelectedListener { menuItem ->
-            // TODO: Handle menu item selected
+            if(menuItem.itemId == R.id.home_item){
+                val homeIntent = Intent(this, StudentActivity::class.java)
+                finish()
+                startActivity(homeIntent)
+            }
             if (menuItem.itemId == R.id.feeback_item) {
                 val feedbackIntent = Intent(this, UserFeedbackActivity::class.java)
                 finish()
